2fd987d8606ec04f81b3583304cb1cc5ff348b8b
Leonna <koicaaaaaa@gmail.com>
2025-04-19 12:04:14 -0400

    The color picker works and the reset button also is now functional. There are no more lines in the grid.

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+const DEFAULT_COLOR = "#000000";
+
+let currentColor = DEFAULT_COLOR;
+
 const container = document.querySelector(".gridContainer");
+const resize = document.querySelector("#resize");
+const colorPicker = document.querySelector("#color");
+const rainbow = document.querySelector("#rainbow");
+const opacity = document.querySelector("#opacity");
+const erase = document.querySelector("#erase");
+const reset = document.querySelector("#reset");
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+reset.addEventListener("click", resetGrid);
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
 
 function createGrid(input) {
     for (i = 0; i < input * input; i++) {
@@ -9,6 +30,24 @@ function createGrid(input) {
         grid.style.height = `${624 / input}px`;
         container.appendChild(grid);
     }
+    let gridPixels = container.querySelectorAll("div");
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener("mouseover", colorGrid));
 }
+
+function colorGrid(e) {
+    if (e.type === 'mouseover' && !mouseDown) {return}
+    else {
+    this.style.backgroundColor = currentColor; }
+}
+
+function resetGrid() {
+    let gridPixels = container.querySelectorAll("div");
+    gridPixels.forEach(element => {
+        element.style.backgroundColor = "#FFFFFF";
+    })
+}
+
+
+
 
 createGrid(16);
